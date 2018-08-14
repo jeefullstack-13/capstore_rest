92c5b4ca07b6f8c0d3a833975b204007b1b95612
Training2017 <vijayalakshmi.david@capgemini.com>
2018-08-14 16:11:25 +0530

    first commit

---
+++ src/main/java/org/capstore/rest/CapstoreRestApplication.java
@@ -0,0 +1,12 @@
+package org.capstore.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoreRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoreRestApplication.class, args);
+	}
+}
