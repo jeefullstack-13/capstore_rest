1e9b5426a4daf37857a0666bc485e594c9349712
saporedd <saporedd@DIN73002999.corp.capgemini.com>
2018-08-18 17:40:08 +0530

    Inventory First Commit

--- src/main/java/org/capstore/rest/service/InventoryService.java
+++ src/main/java/org/capstore/rest/service/InventoryService.java
@@ -5,5 +5,9 @@ import java.util.List;
 import org.capstore.rest.model.Inventory;
 
 public interface InventoryService {
+
+	public void save(Inventory product);
+    public void delete(Integer productId) ;
+    public List<Inventory> getAll();
 	List<Inventory> getAllInventory();
 }
