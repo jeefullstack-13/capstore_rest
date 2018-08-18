adf99dc0c7e6816658bff5a658ee9c26942c9d55
shafemoh <shafemoh@DIN73002751.corp.capgemini.com>
2018-08-18 17:24:11 +0530

    brand table

---
+++ src/main/java/org/capstore/rest/model/Brand.java
@@ -0,0 +1,75 @@
+package org.capstore.rest.model;
+
+
+import java.util.List;
+
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+import javax.persistence.OneToMany;
+
+
+@Entity
+public class Brand {
+@Id
+@GeneratedValue(strategy=GenerationType.AUTO)
+private int brandId;
+private String brandName;
+@OneToMany(targetEntity=Inventory.class, mappedBy="brand")
+private List<Inventory> inventory;
+
+public Brand() {
+super();
+}
+
+
+
+public int getBrandId() {
+return brandId;
+}
+
+public void setBrandId(int brandId) {
+this.brandId = brandId;
+}
+
+public String getBrandName() {
+return brandName;
+}
+
+public void setBrandName(String brandName) {
+this.brandName = brandName;
+}
+
+
+
+public List<Inventory> getInventory() {
+return inventory;
+}
+
+
+
+public void setInventory(List<Inventory> inventory) {
+this.inventory = inventory;
+}
+
+
+
+public Brand(int brandId, String brandName, List<Inventory> inventory) {
+super();
+this.brandId = brandId;
+this.brandName = brandName;
+this.inventory = inventory;
+}
+
+
+
+
+
+
+
+
+}
